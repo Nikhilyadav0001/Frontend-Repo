14d3e3067cfdcec547f042d4b8e87119ea8e1219
Nikhilyadav0001 <nikhilraoshab1@gmail.com>
2026-09-10 22:11:57 +0530

    components

---
+++ React-JS/02-components/src/componemt/card.jsx
@@ -0,0 +1,10 @@
+function card() {
+    return <div className="card">
+        <h1>hi i am a card</h1>
+        <p>lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+    </div>
+}
+
+export default card
+
+// now we have to render the card component in the App.jsx
